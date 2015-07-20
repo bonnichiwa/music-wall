ec5f7fc82ee2799ab9649cf21eb373a5819eff87
bonnichiwa <bonnie.pham@ryerson.ca>
2015-07-20 19:10:02 +0000

    Added styling making it extra pretty

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'sinatra-activerecord'
 gem 'puma'
 gem 'tux'
 
+gem 'autoprefixer-rails'
+
 group :development, :test do
   gem 'pry'
   gem 'shotgun'
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,11 @@
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require_tree .
+
 $(document).ready(function() {
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
