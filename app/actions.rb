78e97c0907ac7527e165e1218e10c459fc7e36b2
bonnichiwa <bonnie.pham@ryerson.ca>
2015-07-20 16:26:56 +0000

    styled a bit and made url a link but doesn't work, song info page works

--- app/actions.rb
+++ app/actions.rb
@@ -8,3 +8,23 @@ get '/songs' do
   erb :'songs/index'
 end
 
+get '/songs/new' do
+  erb :'songs/new'
+end
+
+get '/songs/:id' do
+  @song = Song.find params[:id]
+  erb :'songs/show'
+end
+
+post '/songs' do
+  @song = Song.new(
+    title: params[:title],
+    author: params[:author],
+    url:  params[:url]
+  )
+  @song.save
+  redirect '/songs'
+end
+
+
